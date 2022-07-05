126b61c5aab85686e432bf0e8000bd9381c4cfe6
system205 <arsenmutalapov@gmail.com>
2022-07-05 16:05:48 +0300

    add header and global styles

---
+++ src/components/Header.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	import { createEventDispatcher } from 'svelte';
+
+	export let text: string = 'Arsen Mutalapov';
+	const a = () => {
+		console.log('Clicked');
+	};
+
+	const dispatch = createEventDispatcher<{ page: string }>();
+	function changePage(page: string) {
+		dispatch('page', page);
+	}
+</script>
+
+<header>
+	<h1>{text}</h1>
+	<nav>
+		<a href="{base}/"><button class="menu-item">Intro</button></a>
+		<a href="{base}/about"><button class="menu-item">About</button></a>
+	</nav>
+</header>
+
+<style>
+	h1 {
+		margin: 0;
+		padding: 5px;
+	}
+	header {
+		text-align: center;
+		border-bottom: 3px solid #1b2631;
+		border-radius: 0 0 15px 15px;
+		background: linear-gradient(35deg, #1b2631, #aeb6bf, #ebedef);
+	}
+	.menu-item {
+		border-style: inset;
+		border-width: 2px;
+		border-radius: 5px;
+		transition-duration: 0.3s;
+		height: 100%;
+		background-color: #85929e;
+	}
+	.menu-item:focus {
+		text-shadow: 0 0 5px #aeb6bf;
+	}
+
+	nav {
+		height: 40px;
+		padding: 3px;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Header from '../components/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
 <main>
 	<slot />
 </main>
