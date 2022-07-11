3a124bcb20ff975f5853a635984b84624d806edd
system205 <arsenmutalapov@gmail.com>
2022-07-11 16:42:57 +0300

    simple jest test with eslint and without ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint', 'prettier', 'jest'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -17,6 +17,7 @@ module.exports = {
 		es2021: true,
 		node: true,
 		es6: true,
+		'jest/globals': true,
 	},
 	rules: {
 		'import/prefer-default-export': 'off',
